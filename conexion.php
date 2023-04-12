2c0373e901f1ee3a092e62330650d0b44b560f3f
Banquito14 <leao.perez@tecsup.edu.pe>
2023-04-12 13:58:03 -0500

    version nueva

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 	function conexion(){
-	$host = "host=containers-us-west-20.railway.app";
+	$host = "host=containers-us-west-20.railway.appv";
 	$port = "port=6240";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
